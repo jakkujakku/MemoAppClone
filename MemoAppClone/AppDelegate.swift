55f9ee140181d17228220b78afe8fb6584cd8c7d
jakkujakku <jakkujakku98@gmail.com>
2023-08-10 14:48:23 +0900

    런치스크린 적용 및 & 텍스트뷰 UI 변경

--- MemoAppClone/AppDelegate.swift
+++ MemoAppClone/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UITextView.appearance().tintColor = .systemOrange
-        
+        sleep(1)
         return true
     }
     
